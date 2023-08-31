691744d33316e0ae542e60442f252c06e3afad09
David Császár <dcsaszar@users.noreply.github.com>
2023-08-31 12:30:36 +0200

    Prefer "text" property over "widgets" property

--- src/Components/ScrivitoExtensions/ChatbotTab/widgetlistAttributeNames.js
+++ src/Components/ScrivitoExtensions/ChatbotTab/widgetlistAttributeNames.js
@@ -2,10 +2,18 @@ import { sortBy } from "lodash-es";
 
 export function widgetlistAttributeNames(content) {
   const attributes = content.attributeDefinitions();
+  const attributeNames = Object.keys(attributes);
+
+  if (
+    attributeNames.some(
+      (name) => name === "text" && attributes[name][0] === "html"
+    )
+  ) {
+    return [];
+  }
+
   return sortBy(
-    Object.keys(attributes).filter(
-      (name) => attributes[name][0] === "widgetlist"
-    ),
+    attributeNames.filter((name) => attributes[name][0] === "widgetlist"),
     (name) => name.replace("nav", "0")
   );
 }
