4e1823546e271b125e8e02131e836b13a18c2f9b
David Császár <dcsaszar@users.noreply.github.com>
2024-04-08 18:43:21 +0200

    Include page and widget metadata in HTML input

--- src/Components/ScrivitoExtensions/ChatbotTab/extractHtml.js
+++ src/Components/ScrivitoExtensions/ChatbotTab/extractHtml.js
@@ -2,17 +2,50 @@ import * as Scrivito from "scrivito";
 import { flatWidgets } from "./flatWidgets.js";
 
 export async function extractHtml(obj) {
-  const widgets = await Scrivito.load(() => flatWidgets(obj));
-  const html = widgets
-    .map((w) => {
-      const id = w.id();
-      const widgetClass = w.objClass();
-      const inner = w.get("headline") || w.get("text") || "";
-      const tag = widgetClass.startsWith("Headline") ? w.get("style") : "";
-      return `<widget type="${widgetClass}" id="${id}">${
-        tag ? `<${tag}>` : ""
-      }${inner}${tag ? `</${tag}>` : ""}</widget>`;
-    })
-    .join("\n");
-  return `<html>\n${html}\n</html>`;
+  return Scrivito.load(() => {
+    const widgets = flatWidgets(obj);
+    const html = widgets
+      .map((w) => {
+        const widgetClass = w.objClass();
+        const inner = w.get("headline") || w.get("text") || "";
+        const tag = widgetClass.startsWith("Headline") ? w.get("style") : "";
+        return `  <widget ${getAttributesHtml(w)}>${
+          tag ? `<${tag}>` : ""
+        }${inner}${tag ? `</${tag}>` : ""}</widget>`;
+      })
+      .join("\n");
+    return `<html ${getAttributesHtml(obj)}>\n${html}\n</html>`;
+  });
+}
+
+function getAttributesHtml(content) {
+  const attributes = { id: content.id(), type: content.objClass() };
+
+  Object.entries(content.attributeDefinitions()).forEach(
+    ([attributeName, [attributeType]]) => {
+      if ("link" === attributeType) {
+        attributes[`data-${attributeName}-title`] =
+          content.get(attributeName)?.title() || "";
+      }
+      if (
+        [
+          "boolean",
+          "enum",
+          "float",
+          "integer",
+          "multienum",
+          "string",
+          "stringlist",
+        ].includes(attributeType)
+      ) {
+        attributes[`data-${attributeName}`] = []
+          .concat(content.get(attributeName) ?? "")
+          .join(" ");
+      }
+    }
+  );
+
+  return Object.entries(attributes)
+    .map(([k, v]) => `${k}="${v}"`)
+    .join(" ");
 }
