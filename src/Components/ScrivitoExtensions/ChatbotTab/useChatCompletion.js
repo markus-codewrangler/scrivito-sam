0b667d5700119eaa70ddb261fc69fa854a93e031
Markus Hartig <markus.hartig@justrelate.com>
2025-01-20 21:20:08 +0100

    TMP example tool calls

--- src/Components/ScrivitoExtensions/ChatbotTab/useChatCompletion.js
+++ src/Components/ScrivitoExtensions/ChatbotTab/useChatCompletion.js
@@ -66,39 +66,59 @@ async function startStreaming({
     apiKey: OPENAI_API_KEY || apiKey,
     baseURL: OPENAI_API_KEY
       ? "https://api.openai.com/v1"
-      : "https://e7iuggnyr4t2grfrffawjd2q5a0mdcgh.lambda-url.eu-central-1.on.aws/v1",
+      : "http://localhost:3000/v1", // "https://e7iuggnyr4t2grfrffawjd2q5a0mdcgh.lambda-url.eu-central-1.on.aws/v1",
 
     defaultQuery: { tenant_id: instanceId },
     dangerouslyAllowBrowser: true,
   });
 
-  let stream;
-
   try {
-    stream = await client.chat.completions.create({
-      model,
-      messages,
-      stream: true,
-      user,
-    });
+    const runner = client.beta.chat.completions
+      .runTools({
+        stream: true,
+        model: "aws/anthropic.claude-3-5-sonnet-20240620-v1:0", // "gpt-4o", //
+        messages: [{ role: "user", content: "How is the weather this week?" }],
+        tools: [
+          {
+            type: "function",
+            function: {
+              function: getCurrentLocation,
+              description: "get the current location",
+            },
+          },
+          {
+            type: "function",
+            function: {
+              function: getWeather,
+              parse: JSON.parse,
+              parameters: {
+                type: "object",
+                properties: {
+                  location: { type: "string" },
+                },
+              },
+              description: "get weather for location",
+            },
+          },
+        ],
+      })
+      .on("message", (message) => console.log("message:", message));
+
+    const finalContent = await runner.finalContent();
+    console.log("Final content:", finalContent);
   } catch (error) {
     console.error(error);
     setLoading(false);
     return;
   }
 
-  const message = {};
-
-  for await (const chunk of stream) {
-    const { content, role } = chunk.choices[0].delta;
-    if (role) message.role = role;
-    if (content) {
-      message.content = [message.content, content].join("");
-      setCompletionMessage({ ...message });
-    }
-  }
-
-  setCompletionMessage(null);
-  setMessages(messages.concat({ ...message }));
   setLoading(false);
+}
+
+async function getCurrentLocation(_) {
+  return "Boston"; // Simulate lookup
+}
+
+async function getWeather(_) {
+  return { temperature: "50degF", preciptation: "high" };
 }
